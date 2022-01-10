7a52f2e3962f5d707f756ac8e6df465f5e5be63f
Wilder <friendwilder@yahoo.com.mx>
2022-01-09 19:56:30 -0600

    Add Healthy Sleep

---
+++ Topics/If expression/Check a number is positive/src/Main.kt
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+fun main() {
+    // write your code here
+    val scanner = Scanner(System.`in`)
+
+    val number = scanner.nextInt()
+
+    if (number > 0) {
+        println("YES")
+    } else {
+        println("NO")
+    }
+}
